30e1dbc492bd7589e0b3ea1fae787aa3c6354c5e
brian-ikiara <brnikiara@gmail.com>
2023-12-07 20:39:49 +0300

    [[Update]]: Started off `Comprehensions` :heavy_check_mark:

---
+++ Comprehensions/intPlay/Python/isPalindrome.py
@@ -0,0 +1,34 @@
+"""LeetCode Solution #2.
+
+~~~Is it a Palindrome~~~
+Given an integer x, return true if x is a Palindrome
+and false otherwise.
+
+Case:
+    1. x = 121 => true: reads forwards and backwards as 121
+    2. x = -121 => false: reads backwards as 121-
+    3. x = 10 => false: it just isn't a Palindrome
+
+Constraints:
+    -2^31 <= x <= 2^31 - 1: Make sure it's O(n) or better!
+"""
+
+
+class Solution:
+    """Represents my Solution."""
+
+    def isPalindrome(self, x: int) -> bool:
+        """Determine if a number is a Palindrome.
+
+        Args:
+            x (int): number
+
+        Returns:
+            Confirmation.
+        """
+        if x < 0:
+            return False
+        determinant = list(x)
+        if determinant == determinant[::-1]:
+            return True
+        return False
